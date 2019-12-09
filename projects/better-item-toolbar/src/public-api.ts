e018dae49ecf9ae09c93938b4803ad53ac5d0c7c
tfaster <t.schneller@gmail.com>
2019-12-09 17:45:33 +0100

    fix: change wrong js reference in public-api

--- projects/better-item-toolbar/src/public-api.ts
+++ projects/better-item-toolbar/src/public-api.ts
@@ -2,5 +2,5 @@
  * Public API Surface of better-item-toolbar
  */
 
-export * from './lib/item-toolbar/item-toolbar.component.js';
+export * from './lib/item-toolbar/item-toolbar.component';
 export * from './lib/item-toolbar.module';
