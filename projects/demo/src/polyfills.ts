30f1400645b5fc24fc6dfd6d285e01569411d9ad
tfaster <t.schneller@gmail.com>
2020-05-13 12:09:11 +0200

    chore: update core-js for IE11 support

--- projects/demo/src/polyfills.ts
+++ projects/demo/src/polyfills.ts
@@ -17,7 +17,7 @@
 /***************************************************************************************************
  * BROWSER POLYFILLS
  */
-import 'core-js/es7/array.js';
+import 'core-js/es/array/includes';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
