59edaa8e442610d5e5a29431758e57a8c40c1445
Timo Schneller <t.schneller@gmail.com>
2025-06-18 23:07:54 +0200

    fix: export template interfaces

--- projects/better-item-toolbar/src/public-api.ts
+++ projects/better-item-toolbar/src/public-api.ts
@@ -4,6 +4,9 @@
 export {
   ToolbarTemplateItemWithDropdownComponent
 } from './lib/toolbar-template-item-with-dropdown/toolbar-template-item-with-dropdown.component';
+export {
+  ToolbarTemplateItemWithDropdown, ToolbarTemplateItem, ToolbarItemDropdownConfig, ItemChooserConfig
+} from './lib/toolbar-template-item-with-dropdown';
 export { ItemDropdownController } from './lib/toolbar-template-item-with-dropdown/item-dropdown/item-dropdown-controller';
 export { ToolbarTemplateItemComponent } from './lib/toolbar-template-item/toolbar-template-item.component';
 export { ItemToolbarComponent } from './lib/item-toolbar/item-toolbar.component';
